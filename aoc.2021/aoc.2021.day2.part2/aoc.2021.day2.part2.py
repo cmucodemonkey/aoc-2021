ddb2f5910bcf420c57809af85fc2814ec4742516
Steven Ball <stevenball88@gmail.com>
2021-12-18 22:14:23 -0500

    Finished Day 2.

---
+++ aoc.2021/aoc.2021.day2.part2/aoc.2021.day2.part2.py
@@ -0,0 +1,17 @@
+input_list = open("Input.txt", "r").read().split("\n")
+x_change = 0
+y_change = 0
+aim = 0
+
+for i in range(0, len(input_list)):
+    temp = input_list[i].split(" ");
+
+    if temp[0] == "forward":
+        x_change += int(temp[1])
+        y_change += (aim * int(temp[1]))
+    elif temp[0] == "up":
+        aim -= int(temp[1])
+    elif temp[0] == "down":
+        aim += int(temp[1])
+
+print(x_change * y_change)
