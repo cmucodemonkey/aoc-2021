90059e7822274c4011bc6c67df754b171c86f3fa
Steven Ball <stevenball88@gmail.com>
2021-12-31 09:33:51 -0500

    Completed day 5 part 1

---
+++ aoc.2021/aoc.2021.day5.part1/aoc.2021.day5.part1.py
@@ -0,0 +1,24 @@
+from collections import Counter
+
+input_list = open("Input.txt", "r").read().split("\n")
+coordinate_list = []
+count = 0
+
+for item in input_list:
+    firstPoint, secondPoint = item.split(" -> ")
+    x1, y1 = map(int, firstPoint.split(","))
+    x2, y2 = map(int, secondPoint.split(","))
+
+    if x1 == x2 and y1 != y2:
+        for y in range(min(y1, y2), max(y1, y2) + 1):
+            coordinate_list.append((x1, y))
+    elif x1 != x2 and y1 == y2:
+        for x in range(min(x1, x2), max(x1, x2) + 1):
+            coordinate_list.append((x, y1))
+
+elements_count = Counter(coordinate_list)
+for key, value in elements_count.items():
+    if value > 1:
+        count += 1
+
+print(count)
